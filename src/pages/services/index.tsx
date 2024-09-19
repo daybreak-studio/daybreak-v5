8cd4d6242f683fe4be3301d27815a21008cb3db3
Christopher Ha <contact@bhris.digital>
2024-09-19 12:20:08 -0400

    Fix typo causing build error.
    Renamed Card to card.

--- src/pages/services/index.tsx
+++ src/pages/services/index.tsx
@@ -1,6 +1,6 @@
 import Card from "./components/card";
 import { useState, useRef } from "react";
-import { easeInOut, motion } from "framer-motion";
+import { motion } from "framer-motion";
 
 const cards = [
   { title: "Brand", position: "top-left" },
