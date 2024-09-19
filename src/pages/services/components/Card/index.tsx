bfeba97dc5e300218428e07a14d538269f25a71a
christopher-ha <1christopherha@gmail.com>
2024-09-19 12:22:39 -0400

    Remove eslint override to fix build error.

--- src/pages/services/components/Card/index.tsx
+++ src/pages/services/components/Card/index.tsx
@@ -7,39 +7,39 @@ interface CardProps {
   onClick: () => void;
 }
 
-// eslint-disable-next-line react/display-name
-const Card = forwardRef<HTMLDivElement, CardProps>(
-  ({ title, position, onClick }, ref) => {
-    let transformClasses = "";
+const Card = forwardRef<HTMLDivElement, CardProps>(function Card(
+  { title, position, onClick },
+  ref,
+) {
+  let transformClasses = "";
 
-    switch (position) {
-      case "top-left":
-        transformClasses =
-          "-translate-x-[calc(100%+0.5rem)] -translate-y-[calc(100%+0.5rem)]";
-        break;
-      case "top-right":
-        transformClasses =
-          "translate-x-[0.5rem] -translate-y-[calc(100%+0.5rem)]";
-        break;
-      case "bottom-left":
-        transformClasses =
-          "-translate-x-[calc(100%+0.5rem)] translate-y-[0.5rem]";
-        break;
-      case "bottom-right":
-        transformClasses = "translate-x-[0.5rem] translate-y-[0.5rem]";
-        break;
-    }
+  switch (position) {
+    case "top-left":
+      transformClasses =
+        "-translate-x-[calc(100%+0.5rem)] -translate-y-[calc(100%+0.5rem)]";
+      break;
+    case "top-right":
+      transformClasses =
+        "translate-x-[0.5rem] -translate-y-[calc(100%+0.5rem)]";
+      break;
+    case "bottom-left":
+      transformClasses =
+        "-translate-x-[calc(100%+0.5rem)] translate-y-[0.5rem]";
+      break;
+    case "bottom-right":
+      transformClasses = "translate-x-[0.5rem] translate-y-[0.5rem]";
+      break;
+  }
 
-    return (
-      <motion.div
-        ref={ref}
-        className={`absolute left-1/2 top-1/2 h-1/6 w-1/4 ${transformClasses} flex transform items-center justify-center rounded-xl border-2 bg-zinc-300`}
-        onClick={onClick}
-      >
-        <p>{title}</p>
-      </motion.div>
-    );
-  },
-);
+  return (
+    <motion.div
+      ref={ref}
+      className={`absolute left-1/2 top-1/2 h-1/6 w-1/4 ${transformClasses} flex transform items-center justify-center rounded-xl border-2 bg-zinc-300`}
+      onClick={onClick}
+    >
+      <p>{title}</p>
+    </motion.div>
+  );
+});
 
 export default Card;
