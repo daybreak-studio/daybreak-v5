84dd867971b68324837f41442d6ea9d3205a5786
christopher-ha <1christopherha@gmail.com>
2024-10-02 17:55:22 -0400

    Add navigation background color animation and headings
    Links to other pages now appear with background color animation.

--- src/components/navigation/index.tsx
+++ src/components/navigation/index.tsx
@@ -2,6 +2,8 @@ import React, { useEffect, useRef } from "react";
 import { motion, useAnimate } from "framer-motion";
 import Link from "next/link";
 import Image from "next/image";
+import Logo from "/public/logos/daybreak-icon.svg";
+import Wordmark from "/public/logos/daybreak-wordmark.svg";
 
 export default function Navigation() {
   const [scope, animate] = useAnimate();
@@ -20,23 +22,39 @@ export default function Navigation() {
       );
       await animate([
         [".glyph_container", { x: "0%" }, { duration: 1, ease: "easeInOut" }],
-        [".wordmark", { x: 0 }, { duration: 1, at: "<", ease: "easeInOut" }],
+        [
+          ".wordmark",
+          { x: 0, opacity: 1, marginTop: "1px", color: "" },
+          { duration: 1, at: "<", ease: "easeInOut" },
+        ],
       ]);
       await animate([
         [
           ".logo_container",
           {
-            width: "8rem",
+            width: "6rem",
           },
-          { duration: 2, at: "0", ease: "easeInOut" },
+          { duration: 1.5, at: "0", ease: "easeInOut" },
         ],
-        [scope.current, { transform: "0" }, { duration: 1, ease: "easeInOut" }],
+        [
+          scope.current,
+          { transform: "0" },
+          { duration: 1.5, ease: "easeInOut", at: "<" },
+        ],
       ]);
-      // await animate(
-      //   ".items",
-      //   { width: "100%", opacity: 1 },
-      //   { duration: 1, ease: "easeInOut" },
-      // );
+
+      await animate([
+        [
+          ".items",
+          { width: "auto", opacity: 1 },
+          { duration: 1, ease: "easeInOut" },
+        ],
+        [
+          ".container",
+          { backgroundColor: "rgb(255,255,255,0.5)" },
+          { duration: 0.5, ease: "easeInOut", at: "<" },
+        ],
+      ]);
     };
 
     sequence();
@@ -49,11 +67,11 @@ export default function Navigation() {
       ref={scope}
     >
       <motion.div
-        className="container relative flex w-fit items-center justify-center p-4"
-        initial={{ opacity: 0 }}
+        className="container relative mt-4 flex w-fit items-center justify-center space-x-8 rounded-xl px-6 py-4"
+        initial={{ opacity: 0, backgroundColor: "rgb(255,255,255,0)" }}
       >
         <motion.div
-          className="logo_container align-center relative flex rounded-xl p-4"
+          className="logo_container align-center relative flex rounded-xl"
           initial={{ width: "16rem", height: "auto" }}
         >
           <motion.div
@@ -67,45 +85,42 @@ export default function Navigation() {
               className="glyph h-full w-full origin-bottom"
               initial={{ rotate: 180 }}
             >
-              <Image
-                className="h-full w-full"
-                src="./logos/daybreak-icon.svg"
-                alt="Daybreak"
-                width={32}
-                height={32}
-              />
+              <Logo className="h-full w-full fill-current text-zinc-700" />
             </motion.div>
           </motion.div>
 
           <motion.div
-            className="wordmark_container overflow-hidden pl-[4%] pt-[1%]"
+            className="wordmark_container overflow-hidden pl-[6%] pt-[1%]"
             initial={{ width: "75%" }}
           >
             <motion.div
               className="wordmark h-full w-full"
               initial={{
                 x: "100%",
+                opacity: 0,
               }}
             >
-              <Image
-                className="h-full w-full"
-                src="./logos/daybreak-wordmark.svg"
-                alt="Daybreak"
-                width={128}
-                height={64}
-              />
+              <Wordmark className="h-full w-full fill-current text-zinc-700" />
             </motion.div>
           </motion.div>
         </motion.div>
 
         <motion.div
-          className="items ml-auto space-x-4"
+          className="items space-x-8"
           initial={{ opacity: 0, width: "0" }}
         >
-          <Link href="/services">Services</Link>
-          <Link href="/work">Work</Link>
-          <Link href="/about">About</Link>
-          <Link href="/contact">Contact</Link>
+          <Link href="/services" className="text-sm text-zinc-500">
+            Services
+          </Link>
+          <Link href="/work" className="text-sm text-zinc-500">
+            Work
+          </Link>
+          <Link href="/about" className="text-sm text-zinc-500">
+            About
+          </Link>
+          <Link href="/contact" className="text-sm text-zinc-500">
+            Contact
+          </Link>
         </motion.div>
       </motion.div>
     </motion.nav>
