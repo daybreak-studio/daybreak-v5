6165ce7591766c892baedf90a6df95caf4a9c4e9
christopher-ha <1christopherha@gmail.com>
2024-10-08 14:58:47 -0400

    checkpoint: Add navigation sliding anim

---
+++ src/pages/work/index.tsx
@@ -0,0 +1,9 @@
+import Layout from "@/components/layout";
+
+export default function Work() {
+  return (
+    <Layout>
+      <div className="h-screen w-full bg-blue-500">Work</div>
+    </Layout>
+  );
+}
