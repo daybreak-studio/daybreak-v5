aa66a76ccd2a6750585505bec981c03594f1b1c6
christopher-ha <1christopherha@gmail.com>
2024-10-09 14:59:57 -0400

    Add context for visitor
    Context handles if the user has visited the site before, and this is being used to conditionally control the animation of the navigation.
    
    If the user has visited the site before, the navigation will not animate.
    If the user has not visited the site before, the navigation will animate.
    The content on the home page will have different animation timings based on the user's visit history.
    ie. 1.5s delay on the home page content if the navigation is animating.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,14 +2,17 @@ import "@/styles/globals.css";
 import { AppProps } from "next/app";
 import { AnimatePresence } from "framer-motion";
 import Navigation from "@/components/navigation";
+import { VisitProvider } from "@/contexts/VisitContext";
 
 export default function App({ Component, pageProps, router }: AppProps) {
   return (
-    <main>
-      <Navigation />
-      <AnimatePresence mode="wait">
-        <Component key={router.route} {...pageProps} />
-      </AnimatePresence>
-    </main>
+    <VisitProvider>
+      <main>
+        <Navigation />
+        <AnimatePresence mode="wait">
+          <Component key={router.route} {...pageProps} />
+        </AnimatePresence>
+      </main>
+    </VisitProvider>
   );
 }
