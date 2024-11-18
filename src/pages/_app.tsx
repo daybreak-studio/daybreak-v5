f8a31506dee7b051684a47a906b0c6c4de8d145d
christopher-ha <1christopherha@gmail.com>
2024-11-18 18:56:02 -0500

    Add conditional page route transition animation for subpaths and base routes.
    Only render page route transition between base routes, and for subroutes we dont run any layout transitions.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,13 +4,16 @@ import { AnimatePresence } from "framer-motion";
 import Navigation from "@/components/navigation";
 import { VisitProvider } from "@/contexts/VisitContext";
 import Layout from "@/components/layout";
+import { useBaseRoute } from "@/hooks/useBaseRoute";
 
-export default function App({ Component, pageProps, router }: AppProps) {
+export default function App({ Component, pageProps }: AppProps) {
+  const { isBaseRoute, currentBasePath } = useBaseRoute();
+
   return (
     <VisitProvider>
       <Navigation />
       <AnimatePresence mode="wait">
-        <Layout key={router.route}>
+        <Layout key={currentBasePath}>
           <Component {...pageProps} />
         </Layout>
       </AnimatePresence>
