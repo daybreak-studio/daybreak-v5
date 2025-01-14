26f07700dda7ad7bd8b5f9b83842763244a5f50a
Christopher Ha <63668974+christopher-ha@users.noreply.github.com>
2025-01-14 12:30:40 -0500

    Refactor styles for about modal

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -35,109 +35,145 @@ function PersonInfo({
 }) {
   return (
     <motion.div
-      layout="position"
-      layoutId="person-container"
-      onClick={onToggle}
-      className="flex w-[calc(100vw-2rem)] cursor-pointer flex-col items-center justify-between space-y-4 overflow-hidden border bg-white/60 p-6 backdrop-blur-xl md:w-96"
-      style={{ borderRadius: 16 }}
+      layout
+      layoutId="person-info-root"
+      className="fixed bottom-0 left-0 right-0 z-50 px-3 pb-4 md:bottom-4 md:px-4"
+      style={{ transformOrigin: "bottom center" }}
+      initial={{ opacity: 0, y: 20, scale: 0.9 }}
+      animate={{ opacity: 1, y: 0, scale: 1 }}
+      transition={{
+        duration: 0.4,
+        ease: [0.32, 0.72, 0, 1],
+      }}
     >
-      <AnimatePresence mode="popLayout">
-        {isExpanded ? (
-          <motion.div
-            layout="position"
-            layoutId={person.name}
-            className="flex flex-col text-center"
-          >
-            <motion.h2
-              key={person.name}
-              initial={{ filter: "blur(2px)" }}
-              animate={{ filter: "blur(0px)" }}
-              exit={{ filter: "blur(2px)" }}
-              layout="position"
-              className="whitespace-nowrap font-medium text-stone-500"
-            >
-              {person.name}
-            </motion.h2>
-            <motion.h2
-              key={person.role}
-              initial={{ filter: "blur(2px)" }}
-              animate={{ filter: "blur(0px)" }}
-              exit={{ filter: "blur(2px)" }}
-              layout="position"
-              className="whitespace-nowrap text-stone-400"
-            >
-              {person.role}
-            </motion.h2>
-          </motion.div>
-        ) : (
-          <motion.div
-            layout
-            layoutId={person.name}
-            className="flex w-full items-center justify-between"
-          >
-            <div className="flex flex-col">
+      <motion.div
+        layout
+        layoutId="person-info-container"
+        className="mx-auto h-min w-min overflow-hidden bg-white/40 p-1 drop-shadow-2xl backdrop-blur-md"
+        animate={{
+          borderRadius: isExpanded ? 32 : 16,
+        }}
+        transition={{
+          duration: 0.4,
+          ease: [0.32, 0.72, 0, 1],
+        }}
+        style={{ transformOrigin: "bottom center" }}
+      >
+        <motion.div
+          layout
+          layoutId="person-info-background"
+          onClick={onToggle}
+          className="relative flex w-screen max-w-[calc(100vw-2rem)] cursor-pointer flex-col items-center justify-between space-y-4 overflow-hidden bg-white/30 p-6 backdrop-blur-2xl md:max-w-[400px]"
+          animate={{
+            borderRadius: isExpanded ? 28 : 12,
+          }}
+          transition={{
+            duration: 0.4,
+            ease: [0.32, 0.72, 0, 1],
+          }}
+          style={{ transformOrigin: "bottom center" }}
+        >
+          <AnimatePresence mode="popLayout">
+            {isExpanded ? (
               <motion.div
-                key={person.name}
-                initial={{ filter: "blur(2px)" }}
-                animate={{ filter: "blur(0px)" }}
-                exit={{ filter: "blur(2px)" }}
                 layout="position"
-                className="whitespace-nowrap text-stone-500"
+                layoutId={person.name}
+                className="flex flex-col text-center"
               >
-                {person.name}
-              </motion.div>
-              <motion.div
-                key={person.role}
-                initial={{ filter: "blur(2px)" }}
-                animate={{ filter: "blur(0px)" }}
-                exit={{ filter: "blur(2px)" }}
-                layout="position"
-                className="whitespace-nowrap text-stone-400"
-              >
-                {person.role}
-              </motion.div>
-            </div>
-            <motion.div
-              key={"state: " + isExpanded}
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              exit={{ opacity: 0 }}
-              transition={{ duration: 0.4, delay: 0.4 }}
-            >
-              <ExpandIcon className="h-4 w-4 text-stone-500" />
-            </motion.div>
-          </motion.div>
-        )}
-      </AnimatePresence>
-
-      <AnimatePresence mode="popLayout">
-        {isExpanded && (
-          <motion.div
-            key={person._key}
-            initial={{ opacity: 0, filter: "blur(2px)" }}
-            animate={{ opacity: 1, filter: "blur(0px)" }}
-            exit={{ opacity: 0, filter: "blur(2px)" }}
-            transition={{ duration: 0.2 }}
-            className="flex flex-col space-y-6"
-            layout="position"
-          >
-            <p className="px-8 text-center text-stone-500">{person.bio}</p>
-            <div className="flex items-start gap-1 self-stretch">
-              {person.qaPairs.map((qaPair, index) => (
-                <div
-                  key={index}
-                  className="flex aspect-square h-full w-full flex-col items-center justify-center rounded-2xl border-[1px] border-stone-200/50 bg-stone-200/25 p-4 text-center"
+                <motion.h2
+                  key={person.name}
+                  initial={{ filter: "blur(2px)" }}
+                  animate={{ filter: "blur(0px)" }}
+                  exit={{ filter: "blur(2px)" }}
+                  layout="position"
+                  className="whitespace-nowrap font-medium text-stone-500"
                 >
-                  <div className="pb-1 text-sm text-stone-500">
-                    {qaPair.question}
-                  </div>
-                  <div className="text-md text-stone-700">{qaPair.answer}</div>
+                  {person.name}
+                </motion.h2>
+                <motion.h2
+                  key={person.role}
+                  initial={{ filter: "blur(2px)" }}
+                  animate={{ filter: "blur(0px)" }}
+                  exit={{ filter: "blur(2px)" }}
+                  layout="position"
+                  className="whitespace-nowrap text-stone-400"
+                >
+                  {person.role}
+                </motion.h2>
+              </motion.div>
+            ) : (
+              <motion.div
+                layout
+                layoutId={person.name}
+                className="flex w-full items-center justify-between"
+              >
+                <div className="flex flex-col">
+                  <motion.div
+                    key={person.name}
+                    initial={{ filter: "blur(2px)" }}
+                    animate={{ filter: "blur(0px)" }}
+                    exit={{ filter: "blur(2px)" }}
+                    layout="position"
+                    className="whitespace-nowrap text-stone-500"
+                  >
+                    {person.name}
+                  </motion.div>
+                  <motion.div
+                    key={person.role}
+                    initial={{ filter: "blur(2px)" }}
+                    animate={{ filter: "blur(0px)" }}
+                    exit={{ filter: "blur(2px)" }}
+                    layout="position"
+                    className="whitespace-nowrap text-stone-400"
+                  >
+                    {person.role}
+                  </motion.div>
                 </div>
-              ))}
-            </div>
-          </motion.div>
-        )}
-      </AnimatePresence>
+                <motion.div
+                  key={"state: " + isExpanded}
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  exit={{ opacity: 0 }}
+                  transition={{ duration: 0.4, delay: 0.4 }}
+                >
+                  <ExpandIcon className="h-4 w-4 text-stone-500" />
+                </motion.div>
+              </motion.div>
+            )}
+          </AnimatePresence>
+
+          <AnimatePresence mode="popLayout">
+            {isExpanded && (
+              <motion.div
+                key={person._key}
+                initial={{ opacity: 0, filter: "blur(2px)" }}
+                animate={{ opacity: 1, filter: "blur(0px)" }}
+                exit={{ opacity: 0, filter: "blur(2px)" }}
+                transition={{ duration: 0.2 }}
+                className="flex flex-col space-y-6"
+                layout="position"
+              >
+                <p className="px-8 text-center text-stone-500">{person.bio}</p>
+                <div className="flex items-start gap-1 self-stretch">
+                  {person.qaPairs.map((qaPair, index) => (
+                    <div
+                      key={index}
+                      className="flex aspect-square h-full w-full flex-col items-center justify-center rounded-2xl border-[1px] border-stone-200 bg-stone-400/5 p-4 text-center"
+                    >
+                      <div className="pb-1 text-sm text-stone-500">
+                        {qaPair.question}
+                      </div>
+                      <div className="text-md text-stone-700">
+                        {qaPair.answer}
+                      </div>
+                    </div>
+                  ))}
+                </div>
+              </motion.div>
+            )}
+          </AnimatePresence>
+        </motion.div>
+      </motion.div>
     </motion.div>
   );
 }
@@ -280,13 +316,11 @@ export default function AboutPage({ aboutData }: { aboutData: About }) {
       </div>
 
       {/* Navigation/Bio Card */}
-      <div className="absolute bottom-4 z-10 flex w-full justify-center md:left-4">
-        <PersonInfo
-          person={aboutData.team?.[selectedIndex] as TeamMember}
-          isExpanded={isExpanded}
-          onToggle={() => setIsExpanded(!isExpanded)}
-        />
-      </div>
+      <PersonInfo
+        person={aboutData.team?.[selectedIndex] as TeamMember}
+        isExpanded={isExpanded}
+        onToggle={() => setIsExpanded(!isExpanded)}
+      />
 
       {/* Dots Navigation */}
       {!isExpanded && (
