327972625dc3e6704c71ad959c03c5986bb7210d
Christopher Ha <63668974+christopher-ha@users.noreply.github.com>
2025-03-22 02:44:02 -0400

    Add opacity animation to drawer

--- src/components/scroll-drawer.tsx
+++ src/components/scroll-drawer.tsx
@@ -9,7 +9,7 @@ interface ScrollDrawerProps {
 
 const ScrollDrawer: React.FC<ScrollDrawerProps> = ({ children, className }) => {
   return (
-    <div
+    <motion.div
       className={clsx(
         "relative mt-[93vh] rounded-t-[24px] bg-white/90 shadow backdrop-blur-2xl",
         "overscroll-behavior-none z-[100]",
@@ -18,6 +18,9 @@ const ScrollDrawer: React.FC<ScrollDrawerProps> = ({ children, className }) => {
       style={{
         overscrollBehavior: "none",
       }}
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 0.3 }}
     >
       <div className="absolute left-1/2 top-3 -translate-x-1/2">
         <div className="h-1 w-12 rounded-full bg-neutral-500/15" />
@@ -32,7 +35,7 @@ const ScrollDrawer: React.FC<ScrollDrawerProps> = ({ children, className }) => {
       >
         {children}
       </motion.div>
-    </div>
+    </motion.div>
   );
 };
 
