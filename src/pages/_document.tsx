e0cc331316ed22b6c2c41b949c3e111aa46f7196
Christopher Ha <63668974+christopher-ha@users.noreply.github.com>
2025-03-22 23:06:09 -0400

    Add favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <link rel="icon" href="/favicon.png" />
         {/* <script src="https://unpkg.com/react-scan/dist/auto.global.js" async /> */}
       </Head>
       <body>
